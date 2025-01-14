77e0b8b5f27d5ac59923fdfff1ad44ec9298d7e9
Christian Kim <christiansactualbusiness@gmail.com>
2025-01-14 15:01:33 -0800

    .yml file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from "react";
 import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import './index.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <React.StrictMode>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
+          <Routes>
+            <Route path="*" element={<App />} />
+          </Routes>
+      </BrowserRouter>
+  </React.StrictMode>,
+);
